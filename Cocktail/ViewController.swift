d5222a272ba58a132ef8543a875f9f9e7739a1c5
Andrés Reyes <yoigaiden@gmail.com>
2021-01-14 18:18:47 -0600

    Se implemento el inicio de sesion

--- Cocktail/ViewController.swift
+++ Cocktail/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -17,6 +18,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func inicioSesion(_ sender: UIButton) {
+        if
+            let email = usuarioTextField.text,
+            let password = contraseñaTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password){authResult, error in
+                guard let resultado = authResult, error == nil else{
+                    guard let error = error else{
+                        return
+                    }
+                    self.alerta(texto: error.localizedDescription)
+                    print(error.localizedDescription)
+                    return
+                }
+                print("Ingreso suario \(resultado.user)")
+                self.performSegue(withIdentifier: "inicioSegue", sender: self)
+            }
+        }
+    }
+    
+    func alerta (texto : String){
+        let mensaje :String
+        switch texto {
+        case "The password is invalid or the user does not have a password.":
+            mensaje = "Error en la contraseña"
+        case "There is no user record corresponding to this identifier. The user may have been deleted.":
+            mensaje = "El correo no corresponde a ningun usuario registrado"
+        case "The email address is badly formatted.":
+            mensaje = "Correo invalido"
+        default:
+            mensaje = "falta validacion"
+        }
+        let alerta = UIAlertController(title: "Error", message: mensaje, preferredStyle: .alert)
+        let aceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(aceptar)
+        present(alerta, animated: true, completion: nil)
     }
     
     @IBAction func crearCuenta(_ sender: UIButton) {
